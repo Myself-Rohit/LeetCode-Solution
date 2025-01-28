b5cd212d2a5c5970af1b28f3494f1bd40e28362a
rohit negi <rohitnegi263139@gmail.com>
2025-01-28 21:46:39 +0530

    392. Is Subsequence solution

---
+++ string/392_Is_Subsequence.cpp
@@ -0,0 +1,34 @@
+// 392. Is Subsequence
+
+// Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+
+// A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+
+// Example 1:
+// Input: s = "abc", t = "ahbgdc"
+// Output: true
+
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int i = 0, j = 0;
+        while (i < s.size() && j < t.size()) {
+            if (s[i] == t[j]) {
+                i++;
+            }
+            j++;
+        }
+        return i == s.size();
+    }
+};
+int main() {
+    Solution obj;
+    string s = "abc", t = "ahbgdc";
+    cout << "Input: s = " << s << ", t = " << t;
+    cout << "\nOutput: " << obj.isSubsequence(s, t);
+    return 0;
+}
